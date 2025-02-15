a306e402f1a83f423df9b86d2fe5f1792d88f839
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-02-15 21:05:24 +0530

    Implement getContactById function to return user contact details or a 404 message if no contacts exist

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,6 +1,24 @@
 import { User } from "../models/user.model.js"
 
-export const getContactById = async (req, res) => { }
+export const getContactById = async (req, res) => {
+	const userId = req.params.id
+	const userData = await User.findById(userId).select('-password -whatRole -gender -email -phoneNumber -profileImage')
+	if (userData.contacts.length === 0) {
+		return res.status(404).json({
+			success: false,
+			message: 'Request successful, but no content to return',
+			user: {
+				id: userData._id,
+				name: userData.fullName
+			}
+		})
+	}
+}
+
 export const createNewContent = async (req, res) => { }
+
+
 export const deleteContact = async (req, res) => { }
+
+
 export const updatedContact = async (req, res) => { }
