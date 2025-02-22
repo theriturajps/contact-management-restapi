790e0c9542f384ba67f1fe9ae46f47524c3b6333
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-02-22 16:16:44 +0530

    Update .env.sample to include Cloudinary configuration and simplify MongoDB connection in connectionDb.js

--- src/db/connectionDb.js
+++ src/db/connectionDb.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose"
 
 const dbConnect = async (URI) => {
 	try {
-		await mongoose.connect(URI, {
-			appName: "contact"
-		})
+		await mongoose.connect(URI)
 		console.log('Mongodb database connected successfully !!!')
 	} catch (error) {
 		throw new Error("Database error occurred", { cause: error })
