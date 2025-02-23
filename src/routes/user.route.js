8029c67ea7b96b14d1c5da030f7d883a2a0a1255
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-02-23 16:37:32 +0530

    Remove custom rate limiter middleware and update user routes to eliminate rate limiting for login, signup, and token renewal

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { createRateLimiter } from "../middleware/rateLimit.middleware.js"
 import { multerImage } from "../utils/multerUpload.utils.js";
 import { loginController } from "../controllers/login.controller.js";
 import { signupController } from "../controllers/signup.controller.js";
@@ -9,16 +8,11 @@ import { newRefreshTokens } from "../controllers/user.controller.js";
 
 const userRouter = Router()
 
-const rateLimiter = createRateLimiter({
-	windowMs: 60 * 1000,    // 1 minute
-	maxRequests: 1,       // limit each IP to 100 requests per window
-	message: 'You have exceeded the request limit. Please wait and try again after 1 minute'
-});
 
 
-userRouter.post('/login', rateLimiter, multerImage.none(), loginController)
-userRouter.post('/signup', rateLimiter, multerImage.single('profileImage'), signupController)
-userRouter.post('/newtoken', rateLimiter, checkAuthToken, newRefreshTokens) // renew refresh token
+userRouter.post('/login', multerImage.none(), loginController)
+userRouter.post('/signup', multerImage.single('profileImage'), signupController)
+userRouter.post('/newtoken', checkAuthToken, newRefreshTokens) // renew refresh token
 
 userRouter.delete('/delete', checkAuthToken, deleteUserController)
 
