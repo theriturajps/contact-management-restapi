1a1fa12296ef6bbfd35195a3fdc51c5db28baf01
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-03-02 18:55:20 +0530

    Update nodemailer configuration to use Gmail and enhance sendMail function parameters

--- src/utils/nodeMailer.utils.js
+++ src/utils/nodeMailer.utils.js
@@ -1,19 +1,28 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-	host: "smtp.ethereal.email",
+	host: "smtp.gmail.com",
 	port: 465,
-	secure: true, // true for port 465, false for other ports
+	secure: true,
 	auth: {
 		user: process.env.MAIL_AUTH_USER,
 		pass: process.env.MAIL_AUTH_PASS,
 	},
 });
 
-export const sendMail = async (mailOptions) => {
+
+export const sendMail = async (mailTo, mailSubject, mailHtml) => {
 	try {
-		const info = await transporter.sendMail(mailOptions);
-		console.log("Message sent: %s", info.messageId);
+		const info = await transporter.sendMail({
+			from: {
+				name: "Contact Manager",
+				address: process.env.MAIL_AUTH_USER,
+			},
+			to: mailTo,
+			subject: mailSubject,
+			html: mailHtml
+		});
+		console.log("Message sent: %s", info.response);
 	} catch (error) {
 		console.log(error);
 	}
